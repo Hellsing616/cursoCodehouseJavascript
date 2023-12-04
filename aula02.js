d8968164c0568b7daa383e85c70d7744ce43ba3a
Alex Silva <lex_info@terra.com.br>
2023-12-04 18:28:37 -0300

    AULA02

---
+++ aula02.js
@@ -0,0 +1,9 @@
+// Entraga da aula 02
+var texto = prompt("Digite seu nome:");
+var numero = prompt("Digite sua idade:");
+
+var resultadoConcatenacao = texto + " " + numero;
+var resultadoSoma = parseFloat(numero) + 10;
+
+console.log("Resultado da concatenação:", resultadoConcatenacao);
+console.log("Resultado da soma (+10):", resultadoSoma);
